c3605baced9e09758b6360a3e138d8388f2630f6
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2023-03-04 16:12:20 +0530

    CORS on all origins

--- src/main/java/com/solve/sudoku/controller/SudokuController.java
+++ src/main/java/com/solve/sudoku/controller/SudokuController.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class SudokuController {
     public final static Integer[] ALL_VALID_ENTRY_NUMBER = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    @CrossOrigin(origins = "https://sudoku-puzzle-9x9-presolved-production.up.railway.app")
+    @CrossOrigin(origins = "*")
     @GetMapping("/sudoku")
     public int[][] fillNonMatrixElementsIn9x9Matrix() {
         int[][] matrix9x9WithOnlyDiagonalElements = createBaseMatrixWithDiagonalElements();
